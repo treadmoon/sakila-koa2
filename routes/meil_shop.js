a246e3296e5d6f1b237a9ebebfc5ccac68c584ff
tradmoon <mlvae@qq.com>
2020-03-01 13:44:24 +0800

    swiper

--- routes/meil_shop.js
+++ routes/meil_shop.js
@@ -7,7 +7,15 @@ router.get('/homePageContent', function (ctx, next) {
 })
 
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+  ctx.body = {
+      data:{
+          slides:[
+              {image:'https://pics3.baidu.com/feed/810a19d8bc3eb13599327b0c642ee1d5fc1f4443.jpeg?token=8d78e89dd17f528b50fc501309c35883'},
+              {image:'https://aod-image-material.cdn.bcebos.com/5/pic/daaec11c77685b27fb907da9c0fc685f.jpg'},
+              {image:'https://i0.hdslb.com/bfs/archive/4e777c068dbdffb617a6505b189ea84514b1008c.jpg'},
+          ]
+      }
+  }
 })
 
 module.exports = router
